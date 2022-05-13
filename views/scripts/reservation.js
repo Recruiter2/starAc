2d23723c2b541279bcc72a845d9c86292df6de54
Rambo6Glaz <fake@cia.gov.us>
2022-05-13 17:53:15 +0200

    Reservation fonctionelle

--- views/scripts/reservation.js
+++ views/scripts/reservation.js
@@ -22,13 +22,40 @@ function getSalle(id_salle) {
     return [10, 10]; // Retourne les dimensions de la salle Hauteur, Longueur
 }
 
+function enlevePoints(points){
+    var id_spec = window.sessionStorage.getItem("id_spec");
+    var request ="UPDATE spectateur SET points = points - "+ points +" WHERE id_spec = " + id_spec;
+    db.query(request,
+        function (err, result, fields) {
+            if (err) throw err;
+        }
+    );
+}
+
+var getReservations = function (id_evt) {
+    return new Promise(function (resolve, reject) {
+        var sql = "SELECT * FROM reservation WHERE id_evt = ?";
+        db.query(sql, [id_evt], function (err, result) {
+            if (!err) {
+                resolve(result);
+            } else {
+                resolve({
+                    status: "error",
+                    message: "Error Getting Data",
+                    debug: err
+                });
+            }
+        });
+    });
+};
+
 /**
  * 
  * @param {number} id_salle ID de la salle (si implementé un jour)
  */
-function getReservationTable(id_salle) {
+async function getReservationTable(id_salle) {
 
-    let evt_id = window.sessionStorage.getItem("evt_id");
+    let evt_id = window.sessionStorage.getItem("id_evt");
     if (evt_id == null) {
         alert("Vous êtes arriver sur la page de réservation sans avoir d'ID d'évenement assigné...");
         window.location.assign("spectateur.html");
@@ -38,7 +65,7 @@ function getReservationTable(id_salle) {
     let salle = getSalle(id_salle);
 
     // On génere un tableau représentant les places libres pour la page de réservation
-    let ret = [];
+    var ret = [];
     for (var x = 0; x < salle[0]; x++) {
         ret.push([]);
         for (var y = 0; y < salle[1]; y++) {
@@ -46,5 +73,15 @@ function getReservationTable(id_salle) {
         }
     }
 
+    try {
+        var reservations = await getReservations(evt_id);
+        reservations.forEach(element => {
+            ret[element.posY][element.posX] = 2;
+        });
+        console.log(reservations);
+    } catch (error) {
+        console.log(error);
+    }
+    
     return ret;
 }
